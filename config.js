364b78bd3352a88fad28c524b9caa2277aac5dd7
Arnoud de Boer <a.deboer@geonovum.nl>
2023-05-03 16:33:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var respecConfig = {
-  specStatus: "GN-VV",
+  specStatus: "GN-WV",
   specType: "IM",
   subtitle: "BRO gegevenscatalogus",
-  publishDate: "2023-05-04", 
+  //publishDate: "2023-05-04", 
   logos: [{
     src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
     href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
@@ -41,8 +41,8 @@ var respecConfig = {
   shortName: "GMM",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/GMM",
-  previousPublishDate: "2023-02-01",
-  previousStatus: "GN-CV",
+  previousPublishDate: "2023-05-04",
+  previousStatus: "GN-VV",
   //localBiblio: {
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
